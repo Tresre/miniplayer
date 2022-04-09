a5c5b7555a08bb688ca4222f70cd9b5131a6e893
Tresre <58750285+Tresre@users.noreply.github.com>
2022-04-09 16:01:18 -0400

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -29,6 +29,7 @@ function chooseSong(p) {
     return;
   } else {
     songChoice = randomItem;
+    console.log(playlist.indexOf('songChoice'));
     const splitSong = songChoice.split("$");
     songName = splitSong[0];
     songName = songName.replace(/_/g, " ");
